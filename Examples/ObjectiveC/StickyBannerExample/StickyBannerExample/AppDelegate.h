84bf9cf0a868999dc660be03da64cdb8a12bfe8f
mobile-ads-github <mobile-ads-github@yandex-team.ru>
2020-05-05 12:00:18 +0300

    release sticky banner examples

---
+++ Examples/ObjectiveC/StickyBannerExample/StickyBannerExample/AppDelegate.h
@@ -0,0 +1,14 @@
+/*
+ * Version for iOS © 2015–2020 YANDEX
+ *
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at https://yandex.com/legal/mobileads_sdk_agreement/
+ */
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (nonatomic, strong) UIWindow *window;
+
+@end
