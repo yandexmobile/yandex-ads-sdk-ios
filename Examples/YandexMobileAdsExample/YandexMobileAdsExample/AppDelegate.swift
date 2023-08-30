b7e8ec13793d54a9cc59557ec9b42ea4894f35d6
mobile-ads-github <mobile-ads-github@yandex-team.ru>
2023-08-30 16:35:18 +0200

    Update changlelog and public sample

--- Examples/YandexMobileAdsExample/YandexMobileAdsExample/AppDelegate.swift
+++ Examples/YandexMobileAdsExample/YandexMobileAdsExample/AppDelegate.swift
@@ -1,21 +1,26 @@
 /*
- * Version for iOS © 2015–2021 YANDEX
+ * Version for iOS © 2015–2023 YANDEX
  *
  * You may not use this file except in compliance with the License.
  * You may obtain a copy of the License at https://yandex.com/legal/mobileads_sdk_agreement/
  */
 
-import UIKit
-import GoogleMobileAds
+import YandexMobileAds
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
-    func application(_ application: UIApplication,
-                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
+        YMAMobileAds.initializeSDK()
         return true
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        AppOpenAdController.shared.showAdIfAvailable(from: window)
     }
 }
 
