2196828581f05f9ed6faee905c9a4f1d10baae31
isandbox <isandbox@mobile-mac228.macfarm.yandex.net>
2024-11-11 12:19:15 +0300

    Sign framework

--- Package.swift
+++ Package.swift
@@ -64,13 +64,13 @@ let package = Package(
         ),
         .binaryTarget(
             name: "YandexMobileAds",
-            url: "https://ads-mobile-sdk.s3.yandex.net/Yandex/YandexMobileAds/7.6.2/spm/a0ddcc95-1f92-4786-987d-ffe8e4ce466c.zip",
-            checksum: "bb1b2706d1ae376dfb94f3690a76c0201c64604a6d362aca3412026fb5bb9e3b"
+            url: "https://ads-mobile-sdk.s3.yandex.net/Yandex/YandexMobileAds/7.6.3/spm/1de95683-415d-4666-9154-53300369fb14.zip",
+            checksum: "f2f8db0b4e63485c11e89149d0225a884b86d772ca239d06080db6028e01b7e6"
         ),
         .binaryTarget(
             name: "YandexMobileAdsInstream",
-            url: "https://ads-mobile-sdk.s3.yandex.net/YandexMobileAdsInstream/0.40.1/spm/5d11d7ea-621d-4acd-818d-ce8d55f9d0f7.zip",
-            checksum: "e3ae71e27908fc3f26f25e8aec0d0c92db5a1a3655cd7ac4244fae6115f784d2"
+            url: "https://ads-mobile-sdk.s3.yandex.net/YandexMobileAdsInstream/0.40.2/spm/33d9b311-8495-498a-9a9a-0938d52ea406.zip",
+            checksum: "4bdee6cc3d216dbdccede401cd8a61e94a51dc3c084349c6dad13adcb9b0e46a"
         )
     ]
 )
